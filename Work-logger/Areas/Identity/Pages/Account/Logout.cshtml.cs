2741a4144c9c78b2a4f7cd9b2d3707b841a3c23f
Andrew Witwicki <vitvitskiy3@gmail.com>
2023-11-11 13:41:56 +0100

    Update Logout.cshtml.cs

--- Work-logger/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Work-logger/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -2,31 +2,38 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WorkLogger.Areas.Identity.Pages.Account
+namespace WorkLogger.Areas.Identity.Pages.Account;
+
+public class LogoutModel : PageModel
 {
-    public class LogoutModel : PageModel
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
     {
-        public string ReturnUrl { get; private set; }
-        public async Task<IActionResult> OnGetAsync(
-            string returnUrl = null)
+        _signInManager = signInManager;
+        _logger = logger;
+    }
+
+    private readonly ILogger<LogoutModel> _logger;
+
+    public string ReturnUrl { get; private set; }
+
+    public async Task<IActionResult> OnGetAsync(string returnUrl = null)
+    {
+        await _signInManager.SignOutAsync();
+        _logger.LogInformation("User logged out.");
+        if (returnUrl != null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
-            // Clear the existing external cookie
-            try
-            {
-                await HttpContext
-                    .SignOutAsync(
-                        CookieAuthenticationDefaults.AuthenticationScheme);
-            }
-            catch (Exception ex)
-            {
-                string error = ex.Message;
-            }
+            return LocalRedirect(returnUrl);
+        }
+        else
+        {
+            // This needs to be a redirect so that the browser performs a new
+            // request and the identity for the user gets updated.
             return LocalRedirect("/");
         }
     }
